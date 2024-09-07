d2c29690e2456aed4dddd755db6e36c5b2b75d8b
fluetanol <etanolfly98@gmail.com>
2024-09-08 03:28:36 +0900

    feat) 카메라 스테이지 클러스터별 이동 구현
    1. 카메라가 더 이상 X축으로 이동 불가능한 벽을 마주치면 그 이상으로 이동하지 않는 기능 구현
    
    2. 스테이지 내에 이런 카메라가 못넘어가는 구간을 두어 클러스터를 나누고, 이 클러스터별로 카메라가 이동할 수 있는 범위 제한하는 기능 구현
    
    3. 클러스터를 넘어갔을 때 코루틴을 이용해서 다음 클러스터의 시작 위치로 카메라가 이동하는 애니메이션 추가

--- Assets/Script/Camera/CameraMachine.cs
+++ Assets/Script/Camera/CameraMachine.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions.Must;
@@ -16,39 +17,86 @@ public class CameraMachine : MonoBehaviour
         Smooth,
     }
 
-    public List<Transform> CameraStartBoundaries = new();
-    public List<Transform> CameraEndBoundaries = new();
-
     public ECameraState _cameraState = ECameraState.Follow;
     public ECameraMove _cameraMove = ECameraMove.Smooth;
-    public GameObject Target;
-    public GameObject EndPoints;
-    public float yOffset = 1;
-    public float CameraLerpSpeed = 0.5f;
-    public int clusterNum = 0;
+    public Transform Target;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(_cameraState == ECameraState.Follow) CameraMove();
+    [SerializeField] private float yOffset = 2;
+    [SerializeField] private float _cameraLerpSpeed = 0.5f;
+    [SerializeField] private float _cameraSpeed = 0.5f;
+    private float screenWidth;
+
+    void Awake(){
+        screenWidth = Camera.main.orthographicSize * 2 * Camera.main.aspect;
+
     }
 
-    void CameraMove(){
+    void Update(){
+        if(_cameraState == ECameraState.Follow) CameraMove(_cameraMove);
+
+        CameraPlayerOver();
+    }
+
+
+
+    void CameraMove(in ECameraMove cameraMoveMode){
         Vector2 nextPosition = Vector2.zero;
-        if(_cameraMove == ECameraMove.Linear) nextPosition = Target.transform.position + new Vector3(0, yOffset, -10);
-        else if(_cameraMove == ECameraMove.Smooth) nextPosition = Vector3.Lerp(this.transform.position, Target.transform.position + new Vector3(0, yOffset, -10), CameraLerpSpeed);
+
+        if(cameraMoveMode == ECameraMove.Linear)  nextPosition = Target.position + new Vector3(0, yOffset, -10);
+        else if(cameraMoveMode == ECameraMove.Smooth) nextPosition = Vector3.Lerp(this.transform.position, Target.transform.position + new Vector3(0, yOffset, -10), _cameraLerpSpeed/10);
         
-        float Width = Camera.main.orthographicSize * 2 * Camera.main.aspect;
-
-        if(CameraStartBoundaries[clusterNum].position.x + Width / 2 > nextPosition.x){
-            nextPosition.x = transform.position.x;
-        }
-        else if(CameraEndBoundaries[clusterNum].position.x - Width / 2 < nextPosition.x){
-            nextPosition.x = transform.position.x;
-        }
-
+        if(IsCameraBoundaries(screenWidth, nextPosition)) nextPosition.x = transform.position.x;
         transform.position = new Vector3(nextPosition.x, nextPosition.y, -10);
     }
+
+
+    void CameraPlayerOver(){
+        Vector2 pos = StageManager.Instance.GetCurentEndBoundaries().position;
+        Vector2 pos2 = StageManager.Instance.GetCurrentStartBoundaries().position;
+
+        if (Target.position.x > pos.x){
+            StageManager.Instance.Addcluster();
+            Vector3 Startpos = transform.position;
+            Vector3 Endpos = Startpos + new Vector3(screenWidth, 0, 0);
+            StartCoroutine(CameraClusterMoving(0.2f, Startpos, Endpos));
+        }
+        
+        else if(Target.position.x < pos2.x){
+            StageManager.Instance.Minuscluster();
+            Vector3 Startpos = transform.position;
+            Vector3 Endpos = Startpos - new Vector3(screenWidth, 0, 0);
+            StartCoroutine(CameraClusterMoving(0.2f, Startpos, Endpos));
+        }
+    }
+
+    IEnumerator CameraClusterMoving(float duration, Vector3 Startpos, Vector3 Endpos){
+        _cameraState = ECameraState.Fixed;
+        Vector3 originalPos = transform.localPosition;
+        float elapsed = 0.0f;
+
+        while (elapsed < duration){
+            elapsed += Time.deltaTime;
+            transform.position = Vector3.Lerp(Startpos, Endpos, elapsed / duration);
+            yield return null;
+        }
+        _cameraState = ECameraState.Follow;
+        yield return null;
+    }
+
+
+
+
+    //어떤 바운더리를 넘어가는지 아닌지를 확인 하는 함수. 넘어가면 더 이상 카메라가 움직이지 않게 함 
+    //*stage manager 싱글톤 사용 의존성이 조금 있긴 한데, 특정 함수에만 존재하는 의존성이라 무시해도 된다고 생각함
+    //대신 클래스 응집도는 높혔음
+    bool IsCameraBoundaries(float CameraWidth, Vector2 expectPosition){
+        Vector2 Startpos = StageManager.Instance.GetCurrentStartBoundaries().position;
+        Vector2 Endpos = StageManager.Instance.GetCurentEndBoundaries().position;
+
+        return Startpos.x + CameraWidth / 2 > expectPosition.x || 
+                Endpos.x - CameraWidth / 2 < expectPosition.x;
+    }
+
 
     void OnDrawGizmos(){
         Gizmos.color = Color.red;
---
+++ Assets/Script/Scene/StageManager.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class StageManager : SingletonMonobehavior<StageManager>
+{
+    [SerializeField] private List<Transform> StartBoundaries = new();
+    [SerializeField] private List<Transform> EndBoundaries = new();
+    [SerializeField] private int clusterNum = 0;
+
+    public int Addcluster() => clusterNum++;
+    public int Minuscluster() => clusterNum--;
+    public Transform GetCurrentStartBoundaries() => StartBoundaries[clusterNum];
+    public Transform GetCurentEndBoundaries() => EndBoundaries[clusterNum];
+
+
+
+}
